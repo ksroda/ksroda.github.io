a2a55a32a80c054300e44c15095e485f74c60211
Kamila <kamila.sroda@vp.pl>
2017-05-02 22:18:57 +0100

    auto question change added

--- js/app.js
+++ js/app.js
@@ -237,6 +237,11 @@ function stopQuiz (lastIndex) {
 
 function handleAnswerClicked (questionId, value) {
   userAnswers[questionId] = parseInt(value)
+  console.log(questionId + 1, Object.keys(correctAnswers).length)
+  if (questionId + 1 <= Object.keys(correctAnswers).length) {
+    currentQuestion = currentQuestion + 1
+    moveView(Object.keys(correctAnswers).length)
+  }
 }
 
 function checkScore (lastIndex) {
